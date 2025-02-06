e43199fb5cbd853f9a20f42e06ced0bb67f2c737
NotUnderi <undershotbutton@gmail.com>
2025-02-07 00:31:54 +0200

    Mysql database to add your own car and update the power on it and to compare it to other cars

--- rekkaribotti.py
+++ rekkaribotti.py
@@ -4,9 +4,12 @@ import requests
 from dotenv import load_dotenv
 import discord
 from discord.ext import commands
+import sqlite3
 
+con = sqlite3.connect('autot.db')
+cur = con.cursor()
 
-                         
+  
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
@@ -15,6 +18,14 @@ if TOKEN is None:
 
 intents = discord.Intents.default()
 intents.message_content = True
+
+pattern = re.compile(r'\b[a-zA-Z]{1,3}-?\d{1,3}\b')
+strictPattern = re.compile(r'\b[a-zA-Z]{3}-?\d{3}\b')
+
+def get_all_ids():
+    cur.execute("SELECT id FROM autot")
+    ids = cur.fetchall()
+    return [id[0] for id in ids]
 
 bot = commands.Bot(command_prefix='!', intents=intents)
 
@@ -25,10 +36,48 @@ async def on_ready():
 @bot.command()
 async def ping(ctx):
     await ctx.send('Pong!')
+@bot.command()
+async def auto(ctx):
+    print(ctx.author.id)
+    rekkari = pattern.search(ctx.message.content)
+    if rekkari:
+        try:
+            rekkariRequest = requests.get(f"https://reko2.biltema.com/VehicleInformation/licensePlate/{rekkari.group()}?market=3&language=FI")
+            rekkariJson = rekkariRequest.json()
+            cur.execute("SELECT * FROM autot WHERE id = ?", (ctx.author.id,))
+            print(ctx.author.id)
+            if cur.fetchone():
+                cur.execute("UPDATE autot SET auto = ?, teho = ? WHERE id = ?", (rekkariJson["manufacturer"] + "" + rekkariJson["modelName"], rekkariJson["powerHp"], ctx.author.id))
+                con.commit()
+                await ctx.send("Auto päivitetty tietokannassa \n ")
+                return
+            auto = [ctx.author.id, rekkariJson["manufacturer"] + "" + rekkariJson["modelName"], rekkariJson["powerHp"]]
+            cur.execute("INSERT INTO autot VALUES( ?, ?, ?)",auto)
+            await ctx.send(f"Auto {rekkariJson['manufacturer']} {rekkariJson['modelName']} lisätty tietokantaan")
+            con.commit()
+        except Exception as e:
+            await ctx.send("Rekkaria ei löytynyt")
+            await ctx.send(e)
+    else:
+        ctx.send("Virheellinen rekisterikilpi")
+@bot.command()
+async def autonteho(ctx):
+    try:
+        if ctx.author.id in get_all_ids():
+            new_power = int(ctx.message.content[11:])
+            cur.execute("UPDATE autot SET teho = ? WHERE id = ?", (new_power, ctx.author.id))
+            con.commit()
+            await ctx.send(f"Teho päivitetty: {new_power} hv")
+            return
+        else:
+            await ctx.send("Autoa ei löytynyt")
+    except Exception as e:
+        await ctx.send("Anna uusi teholukema numeroina")
+        await ctx.send(e)
+
 
 @bot.command()
 async def r(ctx):
-    pattern = re.compile(r'\b[a-zA-Z]{1,3}-?\d{1,3}\b')
     rekkari = pattern.search(ctx.message.content)
     if rekkari:
         try:
@@ -42,7 +91,7 @@ async def r(ctx):
 async def on_message(message):
     if message.author == bot.user:
         return
-    strictPattern =  pattern = re.compile(r'\b[a-zA-Z]{3}-?\d{3}\b')
+    strictPattern = re.compile(r'\b[a-zA-Z]{3}-?\d{3}\b')
     rekkari = strictPattern.search(message.content)
     if rekkari:
         try:
@@ -50,6 +99,12 @@ async def on_message(message):
             print(rekkariRequest)
             rekkariJson = rekkariRequest.json()
             await message.channel.send('\n'.join([f"{alala[key]}: **{rekkariJson.get(key, 'N/A')}**" for key in alala]))
+            if message.author.id in get_all_ids():
+                cur.execute("SELECT teho FROM autot WHERE id = ?", (message.author.id,))
+                author_power = cur.fetchone()[0]
+                rekkari_power = rekkariJson["powerHp"]
+                percentage_difference = ((author_power - rekkari_power) / author_power) * 100
+                await message.channel.send(f"Autosi teho: {author_power} hv\nRekkarin teho: {rekkari_power} hv\nTehoero: {percentage_difference:.2f}%")
             print(rekkariJson["manufacturer"])
         except Exception:
             print("Rekkaria ei löytynyt")
