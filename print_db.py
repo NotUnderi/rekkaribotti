75270fc4c709bd2dfa39fee2b325d23ac896ab78
Underi <undershotbutton@gmail.com>
2025-04-03 04:10:16 +0300

    Shuffling loops around

--- print_db.py
+++ print_db.py
@@ -6,19 +6,19 @@ cars.row_factory = sqlite3.Row
 cur = cars.cursor()
 
 print("Owned cars")
-for i in range(5):
-    print("\n")
+
 cur.execute(f"SELECT rekkari FROM autot")
 rows = cur.fetchall()
 
 for row in rows:
     print(dict(row))  # Convert to dictionary and print
-cur.execute(f"SELECT rekkari FROM cache")
-rows = cur.fetchall()
-
-print("Cached cars")
 for i in range(5):
     print("\n")
+
+
+print("Cached cars")
+cur.execute(f"SELECT rekkari FROM cache")
+rows = cur.fetchall()
 for row in rows:
     print(dict(row))  # Convert to dictionary and print
 
