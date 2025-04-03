d8d320a7bf95e8a42715cdc44a0b260210102f89
Underi <undershotbutton@gmail.com>
2025-04-03 04:09:07 +0300

    Add small string to show when tables start and end

--- print_db.py
+++ print_db.py
@@ -1,11 +1,13 @@
 import sqlite3
 
 cars = sqlite3.connect('autot.db')
-cars.row_factory = sqlite3.Row
 
-cars.row_factory = sqlite3.Row
+cars.row_factory = sqlite3.Row 
 cur = cars.cursor()
 
+print("Owned cars")
+for i in range(5):
+    print("\n")
 cur.execute(f"SELECT rekkari FROM autot")
 rows = cur.fetchall()
 
@@ -14,6 +16,9 @@ for row in rows:
 cur.execute(f"SELECT rekkari FROM cache")
 rows = cur.fetchall()
 
+print("Cached cars")
+for i in range(5):
+    print("\n")
 for row in rows:
     print(dict(row))  # Convert to dictionary and print
 
