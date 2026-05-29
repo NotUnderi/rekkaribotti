27cb70b72412f653e2d835bd6f348442ad520426
NotUnderi <undershotbutton@gmail.com>
2026-05-29 10:59:33 +0300

    Added headers to biltema request

--- rekkaribotti.py
+++ rekkaribotti.py
@@ -22,6 +22,19 @@ biltema_db_change = datetime.datetime.fromisoformat("2025-09-08 09:00:00.000000+
 today = datetime.date.today()
 DB_NAME = "autot_new.db"
 
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:151.0) Gecko/20100101 Firefox/151.0",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+    "Accept-Language": "en-US,en;q=0.9",
+    "Sec-GPC": "1",
+    "Upgrade-Insecure-Requests": "1",
+    "Sec-Fetch-Dest": "document",
+    "Sec-Fetch-Mode": "navigate",
+    "Sec-Fetch-Site": "cross-site",
+    "TE": "trailers",
+    "Pragma": "no-cache",
+    "Cache-Control": "no-cache",
+}
 
 with open("config.yaml", "r") as config_file:
     config = yaml.safe_load(config_file)
